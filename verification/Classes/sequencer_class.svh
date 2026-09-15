962e61b8f9e031e12386d468fbebf710fefaeb9a
Ahmed Yasser <ahmedyasser13016@gmail.com>
2026-09-15 07:59:43 +0300

    Delete verification directory

--- verification/Classes/sequencer_class.svh
+++
@@ -1,9 +0,0 @@
-class sequencer_class;
-    sequence_item_class seq_item;
-
-    mailbox #(sequence_item_class) seqr_mb;
-
-    function new ();
-        seqr_mb = new(1);
-    endfunction
-endclass
