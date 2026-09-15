9ff58f7f380a13ccad6d1f18ee64ff41dc79479b
Ahmed Yasser <ahmedyasser13016@gmail.com>
2026-09-15 07:32:13 +0300

    Create package.sv

---
+++ verification/package.sv
@@ -0,0 +1,12 @@
+package pkg;
+    `include "sequence_item_class.svh"
+    `include "sequencer_class.svh"
+    `include "sequence_class.svh"
+    `include "driver_class.svh"
+    `include "monitor_class.svh"
+    `include "agent_class.svh"
+    `include "scoreboard_class.svh"
+    `include "coverage_collector_class.svh"
+    `include "environment_class.svh"
+    `include "test_class.svh"
+endpackage
